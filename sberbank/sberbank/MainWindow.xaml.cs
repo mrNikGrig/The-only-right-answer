17376202e7b9e4e616c88eac45240dc1880d375f
mrNikGrig <48320722+mrNikGrig@users.noreply.github.com>
2022-03-18 09:35:21 +0300

    Add files via upload

---
+++ sberbank/sberbank/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace sberbank
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void ButtonYes_Click(object sender, RoutedEventArgs e)
+        {
+            TextBlok1.Visibility = Visibility.Hidden;
+            TextBlock2.Visibility = Visibility.Visible;
+            ButtonNo.Visibility = Visibility.Hidden;
+            ButtonYes.Visibility = Visibility.Hidden;
+        }
+
+        private void ButtonNo_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Random rand = new Random();
+            int y = rand.Next(0, 400);
+            int x = rand.Next(0, 800);
+            Canvas.SetLeft(ButtonNo, x);
+            Canvas.SetTop(ButtonNo, y);
+        }
+    }
+}
